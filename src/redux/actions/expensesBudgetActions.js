9033502f8af9a6a42c739981cf755133ad8a788f
Timothy Attah <timothyattah733@gmail.com>
2021-03-25 00:13:32 +0100

    Added get api with the server

--- src/redux/actions/expensesBudgetActions.js
+++ src/redux/actions/expensesBudgetActions.js
@@ -2,6 +2,8 @@ import {
   CREATE_EXPENSES_BUDGETS, LIST_EXPENSES_BUDGETS, DELETE_EXPENSES_BUDGET, EDIT_EXPENSES_BUDGET
 } from '../type';
 
+import * as api from '../api';
+
 export const createExpensesBudget = budgets => {
   return {
     type: CREATE_EXPENSES_BUDGETS,
@@ -9,10 +11,16 @@ export const createExpensesBudget = budgets => {
   };
 };
 
-export const listExpensesBudgets = () => {
-  return {
-    type: LIST_EXPENSES_BUDGETS
-  };
+export const listExpensesBudgets = () => async dispatch => {
+  try {
+    const { data } = await api.getExpenses();
+    dispatch( {
+      type: LIST_EXPENSES_BUDGETS,
+      payload: data.expenses
+    } );
+  } catch ( error ) {
+    console.log( error );
+  }
 };
 
 export const deleteExpensesBudget = id => {
--- src/redux/api.js
+++ src/redux/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const incomeUrl = 'http://localhost:8080/api/budgets/incomes';
+const expenseUrl = 'http://localhost:8080/api/budgets/expenses';
 
 export const getIncome = () => axios.get( `${ incomeUrl }` );
 
 export const createIncome = ( newIncome ) => axios.post( `${ incomeUrl }/create/income`, newIncome );
 
 export const deleteIncome = ( id ) => axios.delete( `${ incomeUrl }/delete/income/${ id }` );
+
+export const getExpenses = () => axios.get( expenseUrl );
