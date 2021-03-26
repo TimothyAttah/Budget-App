b7c7e68c8d4df3a7ceb0891712742a4d1d10f40d
Timothy Attah <timothyattah733@gmail.com>
2021-03-26 14:38:38 +0100

    Updated sign-up form authentication

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-alert */
 import { SIGN_UP } from '../type';
 import * as api from '../api';
 
 export const signUpUser = ( users ) => async dispatch => {
   try {
     const { data } = await api.signUpUser( users );
-    dispatch( {
-      type: SIGN_UP,
-      payload: data
-    } );
-    console.log(data);
+    if ( data.error ) {
+      alert( data.error );
+    } else {
+      dispatch( {
+        type: SIGN_UP,
+        payload: data
+      } );
+    }
+    alert( data.message );
+    console.log( data );
   } catch ( error ) {
     console.log( error );
   }
